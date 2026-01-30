d6cf9269f432fa622915df3728e02c63836d5343
Vignesh V M <coopervignesh@gmail.com>
2026-01-30 22:42:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,4 +12,5 @@ def poem():
     return render_template("poem.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
